f654d3927f60845165d9ef1d3761175d5fa70d92
ti17m006 <ti17m006@technikum-wien.at>
2020-07-20 20:21:37 +0200

    app init

--- app01/app.js
+++ app01/app.js
@@ -1,1 +1,14 @@
-console.log('app01');
+const PORT = 8000;
+
+const app = require('express')();
+const exgraph = require('express-graphql');
+const schema = require('./schema/schema');
+
+app
+    .use('/graphql', exgraph.graphqlHTTP({
+        schema,
+        graphiql: true
+    }))
+    .listen(PORT, () => {
+        console.log(`Linstening on a port ${PORT}`);
+    });
