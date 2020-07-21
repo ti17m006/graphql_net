b9ede1d2909568ce0acabcfd366f86498fa5f77b
ti17m006 <ti17m006@technikum-wien.at>
2020-07-21 11:45:17 +0200

    Manager dummy data

---
+++ app02/schema/manager_data.js
@@ -0,0 +1,6 @@
+module.exports.Managers = [
+    { id: 1, fullname: "Alice Martin", username: "artin", password: "123abc" },
+    { id: 2, fullname: "Patrick Foster", username: "pfoster", password: "abc123" },
+    { id: 3, fullname: "Anna Smith", username: "asmith", password: "123456" },
+    { id: 4, fullname: "Jacob Davis", username: "jdavis", password: "qwerty" }
+];
